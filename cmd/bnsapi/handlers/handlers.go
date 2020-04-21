3885d9632260ad080ab960aa3f261e8fe5b3bd0c
Orkun Külçe <orkunkl@users.noreply.github.com>
2020-04-21 12:50:33 +0300

    Rename account/nonce to nonce (#66)

--- cmd/bnsapi/handlers/handlers.go
+++ cmd/bnsapi/handlers/handlers.go
@@ -461,8 +461,6 @@ func (h *BlocksHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type DefaultHandler struct{}
 
 var wEndpoint = []string{
-	"/account/nonce/address/{address}",
-	"/account/nonce/pubkey/{pubKey}",
 	"/cash/balances?address=_[OR]offset=_",
 	"/msgfee/msgfee?msgfee=_",
 	"/username/resolve/{username}",
@@ -619,7 +617,7 @@ type NonceAddressHandler struct {
 // @Success 200
 // @Failure 404
 // @Failure 500
-// @Router /account/nonce/address/{address} [get]
+// @Router /nonce/address/{address} [get]
 func (h *NonceAddressHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	addressStr := LastChunk(r.URL.Path)
 	addr, err := WeaveAddressFromQuery(addressStr)
@@ -655,7 +653,7 @@ type NoncePubKeyHandler struct {
 // @Success 200
 // @Failure 404
 // @Failure 500
-// @Router /account/nonce/pubkey/{pubKey} [get]
+// @Router /nonce/pubkey/{pubKey} [get]
 func (h *NoncePubKeyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	pubKeyStr := LastChunk(r.URL.Path)
 	hexKey, err := hex.DecodeString(pubKeyStr)
